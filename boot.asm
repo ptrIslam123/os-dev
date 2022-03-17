827e6054f1dd40ca9d427dd8f8907a36b241d2fe
Islam <i.kardanov@wisetech.pro>
2022-03-17 14:19:38 +0300

    Пока написал кода для прехода в защищенный режим без вывода

--- boot.asm
+++ boot.asm
@@ -1,37 +1,142 @@
-bits 16
-org 0x7c00
+; 1) Необходимо загрузиться в реальном режиме.
+; 2) Настройть сегментные регистры и стек.
+; 3) Активировать шину адреса А20.
+; 3.1) Дективировать не маскируемые прерывания.
+; 4) Описать дескрипторную таблицу.
+; 5) Загрузить в регистр GDTR адрес на начало дескрипторной таблицы и ее размер.
+; 6) Установить первый бит PE регистра cr0
+; 7) Совершить дальний переход, что бы заставить процессор загрузить в сегментный регистр CS значние смещения(значение индекса в таблице) на 
+; дескриптор описывающий сегмент кода в дескрипторной тблице (селектор): 
+;           jmp [занчение индекса дескриптора в таблице] : [нужное смещение в этом сегменте].
+; 8) Проинициализировать в защищенном режиме остальные сегментные регистры (ds, es, ss, fs, gs) путем простой загрузки в эти регистры значений
+;           индексов(смещения) в дескрипторной таблице, а так же стандартных атриюутов (TI, RPL).
+; 9) Переити к выводу строки приветствия в защищенном режиме.
 
-; На данный момент нельзя размещать строку в определенной секции, иначе почемуто процессор не может получить к этому участку памяти доступ
-; Сейчас строка определена в той же секции что и код програмы.
-mesage db "Hello world!", 0
+use16
+org 0x7C00
+
+__start:
+    jmp far dword 0x0000:setup_segments
+
+setup_segments:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    cli
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+    
+    jmp active_A20
+
+active_A20:
+    in  al, 0x92   
+    or  al, 0x02
+    out 0x92, al    
+
+    ; Деактивация не маскируемых прерываний
+    cli
+    in  al, 0x70
+    or  al, 0x80
+    out 0x70, al
+
+    jmp load_gdt
+
+load_gdt:
+    lgdt [gdt_entry]
+    jmp active_cr0
 
 
-section .text
-global _start
+active_cr0:
+    ; Устанавливаем первый бит(PE - Protect Enable) регистра cr0 в 1 
+    mov  eax, cr0  
+    or   al,  0x01 
+    mov  cr0, eax  
+    ; После установки данного бита мы уже в защищенном режиме и нужно совершить первый дальний переход,
+    ; который позволит записать в сегментный регистр CS индекс на дескриптор сегмента кода в дескрипторной таблице.
+    ; 0000000000001000b is a segment selector which is loaded into CS register
+    ; Segment selector's format:
+    ;  [0:1]  RPL              = 00b            - requested privilege level = 0 (most privileged)
+    ;      2  TI               = 0              - chooses descriptor table; 0 means Global Descriptor Table
+    ; [3:15]  Descriptor Index = 0000000000001b - index of descriptor inside the descriptor table = 1
 
-_start:
+    jmp far dword 0000000000001000b:protection_mode_entry_point
 
-    xor ax, ax ; Зануляем регист
-    mov ds, ax ; Устанавливаем значение сегментного регистра DS(Data segment) сегмент данных
-    mov es, ax ; Устанавливаем значение сегментного регистра ES(Extention segment) расширенный сегмент данных
+use32
+protection_mode_entry_point:
+    mov ax, 0000000000010000b ; segment selector: RPL = 0, TI = 0, Descriptor Index = 2 (второй дескриптор описывает сегмент данных)
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
 
-    mov ah, 0x0e  ; Указываем режим работы BIOS с экраном монитора (он подерживает разные графические и текстовые режимы работы) 
-    mov si, mesage ; Копируем в регистр si (source index индекс источника) адрес начала строки - метка является адресом на начало строки
-    jmp print_str ; Прыгаем на указанную метку
+    jmp kmain
+ 
+ kmain:   
+    cli
+    hlt
+    
 
-print_str: ; Процедура вывода строки на экран средствами BIOS
-    print_cur_char_in_si_if_not_zero:
-        mov al, [si] ; В регист al кладем разыменованный адрес, который хранится в регистре si (текущий символ строки байт)
-        cmp al, 0 ;  Если значение этого байта равно 0
-        je main_loop ; Прыгаем на указанную метку
-        int 0x10 ; Иначе выводим указанный символ в al
-        inc si ; Инкрементив указатель в регистре si на следующий байт
-        jmp print_cur_char_in_si_if_not_zero    ; Прыгаем на начало цикла для вывода на экран следующего (уже текущего) символа строки
+; Выравнивание по границе 8 байт для ускорения доступа к таблице
+align 8
+gdt_begin:
+; Самая сложная часть программы (Нужно определить дескриптоную таблицу с 3 основными сегментами: 0 (первый) дескриптор не используется и к нему
+; нельзя обращаться, 1 (второй) дскриптор будет описывать сегмент кода, 2 (третий) дескриптор будет описывать сегмент данных). Можно и больше
+; дескрипторов, но дляминимальной работы нам этого хватит.
 
-main_loop:
-    jmp main_loop   ; $ - Указатель на текущую инструкцию, данная конструкция означает вечный цикл  
+; Первый дескриптор (всегда нулевой и не используется)
+NullDescriptor: db 8 dup(0)
 
+; Второй дескриптор для сегмента кода
+DescriptorForSegmentCode:
+    dw 0xFFFF           ; Limit[0:15]
+    db 0x00, 0x00, 0x00 ; Base[0:23]
+    db 10011010b        ; P DPL[0:1] S Type[0:3]
+    db 11001111b        ; G D/B L AVL Limit[16:19]
+    db 0x00             ; Base[24:31]
 
-times 510 - ($ - $$) db 0 ; Заполняем оставшуюся память в программе (в секторе 512 байт) 0-ми
+    ; Detailed description of the segment descriptor:
+    ; Base  = 0x00000000 - segment base address = 0
+    ; Limit = 0xFFFFF    - segment size = 2^20
+    ; P     = 1          - presence: segment is present in physical memory
+    ; DPL   = 00b        - descriptor privilege level = 0 (most privileged)
+    ; S     = 1          - system (TSS segment): segment is not a system segment
+    ; Type  = 1010b      - code segment (1), C=0 R=1 A=0 execution and reading allowed
+    ; G     = 1          - granularity: the size of the segment is measured in 4 kilobyte pages, i. e. it's equal to 2^20*4 KiB = 4 GiB
+    ; D/B   = 1          - default size: operands and addresses are 32-bit wide
+    ; L     = 0          - 64-bit code segment: in protected mode this bit is always zero
+    ; AVL   = 0          - available: it's up to the programmer how to use this bit
 
-dw 0xaa55  ; Последние 2 байта должны содержать магические числа, так BIOS определяет какой 1-й сектор является загрузочным
+; Третий дескриптор для сегмента данных (ds, es, ss, fs, gs)
+DescriptorForSegmentData:
+    dw 0xFFFF           ; Limit[0:15]
+    db 0x00, 0x00, 0x00 ; Base[0:23]
+    db 10010010b        ; P DPL[0:1] S Type[0:3]
+    db 11001111b        ; G D/B L AVL Limit[16:19]
+    db 0x00             ; Base[24:31]
+
+    ; Detailed description of the segment descriptor:
+    ; Base  = 0x00000000 - segment base address = 0
+    ; Limit = 0xFFFFF    - segment size = 2^20
+    ; P     = 1          - presence: segment is present in physical memory
+    ; DPL   = 00b        - descriptor privilege level = 0 (most privileged)
+    ; S     = 1          - system (TSS segment): segment is not a system segment
+    ; Type  = 0010b      - data segment (0), E=0 W=1 A=0 reading and writing are allowed, expand-up data segment (offset ranges from 0 to Limit)
+    ; G     = 1          - granularity: the size of the segment is measured in 4 kilobyte pages, i. e. it's equal to 2^20*4 KiB = 4 GiB
+    ; D/B   = 1          - default size: stack pointer is 32-bit wide (concerns stack segment) and the upper bound of the segment is 4 GiB (concerns data segment)
+    ; L     = 0          - 64-bit code segment: in protected mode this bit is always zero
+    ; AVL   = 0          - available: it's up to the programmer how to use this bit
+
+; Определяем размер таблицы дескрипторов
+gdt_size equ $ - gdt_entry
+
+; Указатель на размер(16 - бит) и начало(32 - бита) дескрипторной таблицы
+gdt_entry:
+    dw gdt_size - 1
+    dd gdt_begin
+
+signatura_for_BIOS:
+    times 510-signatura_for_BIOS +__start db 0
+    db 55h, 0AAh
+
