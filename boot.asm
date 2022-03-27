3553d5388ff13c04701d02db9b6bddef695c8892
Islam <i.kardanov@wisetech.pro>
2022-03-27 16:53:04 +0300

    Написал процедуру для чтения сдиска, пока не оч работате

--- boot.asm
+++ boot.asm
@@ -3,8 +3,9 @@ org 0x7c00
 
 
 primary_msg: db "============ Start OS ============", 0
-other_msg: db "Hello world in my OS!", 0
-
+disk_error_info: db "disk error!", 0
+readed_data_from_disk: db "Readed data from Disk!", 0
+module_address equ 0x1000 
 
 _start:
 
@@ -17,18 +18,48 @@ mov sp, bp
 mov ah, 0x0e 
 
 mov si, primary_msg
-call strlen
-
-mov si, primary_msg
-mov di, dx 
+mov di, 34 
 call print
 
-mov si, other_msg
-mov di, 22
-call print
+mov bx, module_address
+mov dh, 2
+call readf
+
+call module_address
 
 main_loop:
     jmp main_loop   ; $ - Указатель на текущую инструкцию, данная конструкция означает вечный цикл  
+
+
+
+; readf()
+readf:
+    push bp
+    mov bp, sp
+
+    mov ah, 0x02 ; режим чтения с диска
+    mov al, dh  ; количество считываемых сектором на диске
+    mov ch, 0x00; номер цилиндра
+    mov cl, 0x02; Номер сектора с которого начинается чтения остальных секторов
+    mov dh, 0x00; Номер считывющей головки диска
+    ; mov dl, dl; Номер устройства (идентификатор дсика). Его устанавливать незачем, читаем с тогоже утстройтсва 
+    ; с которого загрузились
+    int 0x13 ; Прерывание BIOS`а на чтение с диска
+    jc .disk_error
+
+    jmp .exit
+    
+    .disk_error:
+        mov si, disk_error_info
+        mov di, 12
+        call print
+    .exit:
+        mov si, readed_data_from_disk
+        mov di, 15
+        call print
+        mov sp, bp
+        pop bp
+        ret
 
 
 ; print(*message : si, len : di) -> void
@@ -75,3 +106,15 @@ strlen:
 times 510 - ($ - $$) db 0 ; Заполняем оставшуюся память в программе (в секторе 512 байт) 0-ми
 
 dw 0xaa55  ; Последние 2 байта должны содержать магические числа, так BIOS определяет какой 1-й сектор является загрузочным
+
+
+second_sector:
+    mov ah, 0x0e
+    mov al, 'X'
+    int 0x10
+    mov al, 'X'
+    int 0x10
+    mov al, 'X'
+    int 0x10
+
+    jmp $
