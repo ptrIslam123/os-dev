d799dd61ffccb88568e7dc76ee781af85d6cd751
Islam <i.kardanov@wisetech.pro>
2022-04-05 12:52:31 +0300

    Дополнил код для вывода строки из загруженных данных со второго сектора

--- boot.asm
+++ boot.asm
@@ -17,14 +17,38 @@ mov cl, 2 ; Номер считываемого сектора
 mov dl, [BOOT_DISK] ; Номер дискового устроцства с которого будет производится чтение
 int 0x13 ; Прерывание для работы с диском средаствами bios`а
 
-mov ah, 0x0e
-mov al, [0x7e00]
-int 0x10
+mov ah, 0x0e ; Перекдючаем bios на работу дисплея в тестовом режиме
+
+mov si, 0x7e00 ; Передаем в si адрес начала на память со считанными с диска данными
+call print_str ; Вызываем функцию для вывода данных экран
+
 jmp $
+
+; print_str(si : char *str)
+print_str:
+    push bp
+    mov bp, sp
+    
+    .print_iter:
+        mov al, [si]
+        cmp al, 0
+        je .exit
+
+        int 0x10
+        inc si
+        dec di
+        jmp .print_iter 
+
+    .exit:
+        mov sp, bp
+        pop bp
+        ret
+
 
 BOOT_DISK: db 0
 
 times 510-($-$$) db 0              
 dw 0xaa55
 
-times 512 db 'A'
+; ============================================== Start Second Sector ====================================
+second_sector: db "ABCDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA Hello from second sector!", 0
